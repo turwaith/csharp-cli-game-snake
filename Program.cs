da7bdd74d9567d62343771513dda7642d32778a7
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2020-10-25 19:14:15 +0100

    intial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_game_snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
